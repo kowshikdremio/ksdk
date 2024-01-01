e688227b55fb9c131918f2983700281082fdd1b9
kowshikdremio <kowshik.dutta@dremio.com>
2024-01-01 18:15:20 +0530

    Added the client code

--- samplecode/clientapp.py
+++ samplecode/clientapp.py
@@ -6,18 +6,10 @@ import csv
 # Get the connection object
 dremioserver = 'ec2-3-84-218-11.compute-1.amazonaws.com'
 flightport   = '32010'
-username     = 'localadmin'
-pat          = '6nnlXLCBSWy593ceX/4F4+w0geFTmkfnPtlndLAphTu/MAyPRlFI2Ku7GZrODg=='
-flightjarpath = '/Users/kowshikdutta/mycode/flight/jar/flight-sql-jdbc-driver-10.0.0.jar'
+username     = '<User ID>'
+pat          = '<PAT TAKOEN>'
 dremioUrl = 'http://'+dremioserver + ':9047'
 print(dremioUrl)
-
-#cnxn = dremio.getConnection(dremioserver,flightport,username,pat,flightjarpath)
-
-#all_pds_df = dremio.getPDS(cnxn)
-#dremio.closeConnection(cnxn)
-
-#all_pds_df.to_csv("all_pds.csv",index=False,quoting=csv.QUOTE_NONE)
 
 auth_token = dremio.login(username,pat,dremioUrl)
 print(auth_token)
